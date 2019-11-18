9a0f523b8a28303ea8dcd03a6be89a72f4707962
Sudhir Kumar Sharma <sudhir.kumar@crmnext.in>
2019-11-19 01:37:39 +0530

    Updates

---
+++ precache-manifest.f4400df299c72ac10e486a4e7e998247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "c0433e969fc1b3bcb8f1",
+    "url": "/me/static/js/main.c0433e96.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c0433e969fc1b3bcb8f1",
+    "url": "/me/static/css/main.fdaca91b.chunk.css"
+  },
+  {
+    "revision": "7650e1c75e68fdc4ac6a3cac2d41207c",
+    "url": "/me/index.html"
+  }
+];
